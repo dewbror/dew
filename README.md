31c4d52eb090014cbd4fd2b1bd181233b52f8dc7
dewb <dewbror@gmail.com>
2026-05-30 11:37:22 +0200

    Create dewargs.h and update README

--- README.md
+++ README.md
@@ -1,36 +1,22 @@
-DEWLOG 0.4
-===========
+dew
+===
+Small header-only C libraries (C99, C++11 tested).
 
-C/C++ header-only logging library.
+Headers
+-------
+- `dewlog.h`  — logging
+- `dewargs.h` — CLI argument parsing
 
-TODO
-----
-- Write "Documentation" section in header
-- Add thread safety (pthreads, c11-threads, SDL threads?)
-
-How to Use
-----------
-In exactly ONE C/C++ file, do this:
-```c
-#define DEWLOG_IMPLEMENTATION
-#include <dewlog.h>
-```
-This will include all the function prototypes and definitions into that C/C++ file.
-
-How to Build and Run Tests (with cmake)
----------------------------------------
-
-```
-mkdir build
-cd build
+Build and Test
+--------------
+~~~
+mkdir build && cd build
+>>>>>>> 9b5e843 (Create dewargs.h and update README)
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build .
-./Debug/dewlog_tests
-```
+ctest --output-on-failure
+~~~
 
 License
 -------
-
-The source code in this repository is licensed under the MIT License, see [LICENSE.txt](https://github.com/dewbror/dewlog/blob/master/LICENSE.txt). This license applies only to dewlog.h.
-
---- end of README ---
+MIT — see [LICENSE.txt](LICENSE.txt).
---
+++ dewargs.h
@@ -0,0 +1,55 @@
+/*
+ * dewargs.h - small header-only CLI argument parsing library for C and C++
+ * version 0.1
+ *
+ * Do this:
+ *     #define DEWARGS_IMPLEMENTATION
+ * before including this file in *one* C or C++ file to create the implementation.
+ *
+ * Example: TODO: write this
+ *
+ * License:
+ *     MIT. See the end of this file.
+ */
+
+#ifndef DEWARGS_H_
+#define DEWARGS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef DEWARGS_IMPLEMENTATION
+
+#endif // DEWARGS_IMPLEMENTATION
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DEWARGS_H_
+
+/* Version History:
+ *     0.1 (2025-10-01) First released version.
+ */
+
+/* Copyright (c) 2025 dewbror <dewbror@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the “Software”), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
